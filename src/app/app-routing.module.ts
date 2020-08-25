b8136a5207979ee4de5753d3ea9097f121de0171
Pichamon Phatcharasirinawakun <pichada1653@outlook.com>
2020-08-25 10:40:13 +0700

    Add login, register2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { ServiceRegisterComponent } from './pages/service-register/service-register.component';
+import { UserRegisterComponent } from './pages/user-register/user-register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'user-register', component: UserRegisterComponent },
+  { path: 'service-register', component: ServiceRegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UserRegisterComponent } from './pages/user-register/user-register.component';
+import { ServiceRegisterComponent } from './pages/service-register/service-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserRegisterComponent,
+    ServiceRegisterComponent
   ],
   imports: [
     BrowserModule,
